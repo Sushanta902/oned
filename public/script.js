b5736c118d2468b1e842959290d325ccfb6f0bc2
Sushanta Neupane <sushantaneupane123@gmail.com>
2023-03-13 08:13:12 +0545

    changed

---
+++ public/script.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    Particles.init({
+      selector: '.background',
+      color : '#1C2833',
+      connectParticles : 'true',
+      minDistance : '110',
+      speed:.7,
+      sizeVariations:1,
+      maxParticles:150,
+
+    });
+  };
